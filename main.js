468a4c97957ed60845fb9e467fe26577751cc89d
JoelSRJE <JoelSRJE@hotmail.com>
2023-06-20 09:22:21 +0200

    create cards js

--- main.js
+++ main.js
@@ -16,43 +16,64 @@ Avklarad datum
 Om du går för VG och använder dummyjson API:et så använder du deras todo objekt-struktur.
 */
 
-// Documented Object Model
-const create = document.querySelector(".create-section");
-const task_wrapper = document.querySelector(".task-container");
+//Documented Object Model after the creation-function
+const form = document.querySelector(".create-form")
+const list = document.querySelector(".task-list")
+const userInput = document.querySelector(".create-input");
+const userDescription = document.querySelector(".create-description");
 
-function creation() {
-    // The creation area
-    const create_title = document.createElement("h1");
-    create_title.classList.add("create-title");
-    create_title.innerText = "To Do It";
+// Give the cards unique ID, starting from 1.
+let creationCount = 1;
 
-    const create_form = document.createElement("form");
-    create_form.classList.add("create-form");
+// Addeventlistener for the form
+form.addEventListener("submit", (e) => {
+     e.preventDefault();
+     
+     const input = userInput.value;
+     const description = userDescription.value;
 
-    const create_input = document.createElement("input");
-    create_input.classList.add("create-input");
-    create_input.placeholder = "Write the title";
-    create_input.setAttribute("required", "required")
+     const todo_card = document.createElement("div");
+     todo_card.classList.add("created-card")
+     todo_card.setAttribute("Id", creationCount)
+     creationCount++;
 
-    const create_description = document.createElement("textarea");
-    create_description.classList.add("create-description");
-    create_description.type = "text";
-    create_description.placeholder = "Write the description";
-    create_description.rows = 4;
-    create_description.cols = 10;
+     const todo_container = document.createElement("div");
+     todo_container.classList.add("card-content")
+     todo_card.appendChild(todo_container);
 
-    const create_button = document.createElement("button");
-    create_button.classList.add("create-button");
-    create_button.innerHTML = "<img src= 'images/pen.png'/>"
-    create_button.setAttribute("submit", "submit");
+     // The users title of the task.
+     const todo_input = document.createElement("input");
+     todo_input.classList.add("created-input");
+     todo_input.type = "text";
+     todo_input.value = input;
+     todo_input.setAttribute("readonly", "readonly");
+     todo_container.appendChild(todo_input);
 
-    // Appends
-    create.appendChild(create_form);
-    create_form.appendChild(create_title);
-    create_form.appendChild(create_input);
-    create_form.appendChild(create_description);
-    create_form.appendChild(create_button);
-}
+     // The users description of the task.
+     const todo_description = document.createElement("textarea");
+     todo_description.classList.add("created-description");
+     todo_description.value = description;
+     todo_description.setAttribute("readonly", "readonly");
+     todo_container.appendChild(todo_description);
 
-//Calls the function creation to generate the entire form which will be used.
-creation();
+     // Button to hightlight that the task is done.
+     const todo_done_button = document.createElement("button");
+     todo_done_button.classList.add("done-btn")
+     todo_container.appendChild(todo_done_button);
+
+     // Div to easier control the actions of the buttons below.
+     const todo_actions = document.createElement("div");
+     todo_actions.classList.add("actions");
+     todo_card.appendChild(todo_actions);
+
+     // Buttons to edit / delete the tasks within the list.
+     const todo_edit_button = document.createElement("button");
+     todo_edit_button.classList.add("edit-btn");
+     todo_actions.appendChild(todo_edit_button);
+
+     const todo_delete_button = document.createElement("button");
+     todo_delete_button.classList.add("delete-btn");
+     todo_actions.appendChild(todo_delete_button);
+        
+     list.appendChild(todo_card);
+});
