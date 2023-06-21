c445fa55a2c8a13812ce5e1176fbdf04616da90d
JoelSRJE <JoelSRJE@hotmail.com>
2023-06-21 14:17:37 +0200

    added timestamps for create&done

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 /* 
 Du ska i denna uppgift bygga en todoapp. Appen ska byggas med HTML & CSS, tillsammans med JavaScript för att hantera de dynamiska värdena. Följande funktionalitet måste implementeras:
 
-Skapa todo med titel och beskrivning
-Lista upp färdiga- och ofärdiga todos
-Avklara todo (check av)
-Ta bort todo
+Skapa todo med titel och beskrivning  <!--Check-->
+Lista upp färdiga- och ofärdiga todos <!--Check-->
+Avklara todo (check av) <!--Check-->
+Ta bort todo <!--Check-->
 
 En Todo ska minst bestå av
-Titel
-Beskrivning
-Avklarad (boolean)
+Titel <!--Check-->
+Beskrivning <!--Check-->
+Avklarad (boolean) 
 Skapad datum
 Avklarad datum
 
@@ -21,9 +21,17 @@ const form = document.querySelector(".create-form")
 const list = document.querySelector(".task-list")
 const userInput = document.querySelector(".create-input");
 const userDescription = document.querySelector(".create-description");
+const listWrap = document.querySelector(".list-wrapper");
+
+// Creates a list for the cards that are done
+const done_list = document.createElement("ul");
+done_list.classList.add("done-list")
+listWrap.appendChild(done_list);
+
+const doneTodos = [];
 
 // Give the cards unique ID, starting from 1.
-let creationCount = 1;
+let cardCount = 1;
 
 // Places stuff in array
 const todoArray = [];
@@ -37,8 +45,8 @@ form.addEventListener("submit", (e) => {
 
      const card = document.createElement("div");
      card.classList.add("created-card")
-     card.setAttribute("Id", creationCount)
-     creationCount++;
+     card.setAttribute("Id", cardCount)
+     cardCount++;
 
      const container = document.createElement("div");
      container.classList.add("card-content")
@@ -59,9 +67,26 @@ form.addEventListener("submit", (e) => {
      card_description.setAttribute("readonly", "readonly");
      container.appendChild(card_description);
 
+     // timestamp container
+     const time_container = document.createElement("div");
+     time_container.classList.add("time-container");
+     container.appendChild(time_container)
+
+     // timestamp of creation (time)
+     const time_stamp = document.createElement("p");
+     time_stamp.classList.add("time-stamp");
+     time_stamp.innerText = getTime();
+     time_container.appendChild(time_stamp);
+
+     // timestamp of creation (date)
+     const time_stamp_date = document.createElement("p");
+     time_stamp_date.classList.add("time-stamp-date");
+     time_stamp_date.innerText = getDate();
+     time_container.appendChild(time_stamp_date)
+
      // Div to easier control the actions of the buttons below.
      const actions = document.createElement("div");
-     actions.classList.add("actions");
+     actions.classList.add("action-bar");
      card.appendChild(actions);
 
     // Button to hightlight that the task is done.
@@ -76,34 +101,85 @@ form.addEventListener("submit", (e) => {
      edit_button.innerHTML = "<img src= images/edit.png>";
      actions.appendChild(edit_button);
 
-
      const delete_button = document.createElement("button");
      delete_button.classList.add("delete-btn");
      delete_button.innerHTML = "<img src= images/delete.png>"
      actions.appendChild(delete_button);
         
+     // Done timestamp, only append when done_button is pressed.
+     const completed_container = document.createElement("div");
+     completed_container.classList.add("completed");
+
+     const completed_stamp = document.createElement("p");
+     completed_stamp.classList.add("completion-timestamp");
+     completed_container.appendChild(completed_stamp);
+
+     /* I could recreate the time_stamp_date to print it out as well
+     but decided not to. Make it less cluttered with info.
+     */
+
      list.appendChild(card);
+
+     done_button.addEventListener("click", () => {
+          if( card.classList == "created-card" ) {
+               card.classList.add("done");
+               done_button.classList.add("toggle-btn-color")
+               card.appendChild(completed_container)
+               completed_stamp.innerText = `Completed: ${getTime()}`;
+          } else {
+               card.classList.remove("done");
+               done_button.classList.remove("toggle-btn-color")   
+               card.removeChild(completed_container);      
+          }
+     });
 
      edit_button.addEventListener("click", (e) => {
           if ( edit_button.classList == "edit-btn" ) {
                edit_button.innerHTML = "<img src= images/save.png>";
                edit_button.classList.add("changed")
-               input.removeAttribute("readonly");
-               description.removeAttribute("readonly");
+               card_input.removeAttribute("readonly");
+               card_description.removeAttribute("readonly");
           } else {
                edit_button.classList.remove("changed");
                edit_button.innerHTML = "<img src= images/edit.png>";
-               input.setAttribute("readonly", "readonly");
-               description.setAttribute("readonly", "readonly");
+               card_input.setAttribute("readonly", "readonly");
+               card_description.setAttribute("readonly", "readonly");
           }
      })
 
      delete_button.addEventListener("click", () => {
           list.removeChild(card);
+          todoArray.remove(card)
      });
 
      // To see that everything works, will remove
-     todoArray.push("submit")
+     todoArray.push(card)
      console.log(todoArray)
      console.log("Signal is going through")
-});
+});     
+
+function getDate() {
+     let date = new Date();
+     let today = date.getDate();
+     let month = date.getMonth() + 1;
+     let year = date.getFullYear();
+ 
+     let currentDate = year + " / " + month + " / " + today;
+
+     return currentDate;
+}
+
+function getTime() {
+     let time = new Date();
+     let hour = time.getHours();
+     let minutes = time.getMinutes();
+     let seconds = time.getSeconds();
+ 
+     hour = hour < 10 ? "0" + hour : hour;
+     minutes = minutes < 10 ? "0" + minutes : minutes;
+     seconds = seconds < 10 ? "0" + seconds: seconds;
+ 
+     let clock = hour + ":" + minutes + ":" +seconds;
+
+     return clock;
+}
